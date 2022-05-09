6d768e6094cfbbde94ef1557ee574ea4ce811b28
Pavol Hejny <me@pavolhejny.com>
2022-05-09 15:43:14 +0200

    Assets are used from inside of repository

--- src/MontessoriItems.tsx
+++ src/MontessoriItems.tsx
@@ -1,10 +1,5 @@
+import { ITrayDynamicItemList, ITrayDynamicToolbarItems, NOT_CONSTRUCTABLE, Translate } from '@collboard/modules-sdk';
 import React from 'react';
-import { NOT_CONSTRUCTABLE } from '@collboard/modules-sdk';
-import {
-    ITrayDynamicItemList,
-    ITrayDynamicToolbarItems,
-} from'@collboard/modules-sdk';
-import { Translate } from '@collboard/modules-sdk';
 import { itemsArrows, toolbarArrows } from './items/arrows';
 import { itemsEvenOdd, toolbarEvenOdd } from './items/evenOdd';
 import { itemsFractions, toolbarFractions } from './items/fractions';
@@ -15,6 +10,17 @@ import { itemsPearls, toolbarPearls } from './items/pearls';
 import { itemsRedBlueBars, toolbarRedBlueBars } from './items/redBlueBars';
 import { itemsSeguinBoard, toolbarSeguinBoard } from './items/seguinBoard';
 import { itemsStripSum, toolbarStripSum } from './items/stripSum';
+
+import arrows from '../assets/tray/categories/arrows.svg';
+import evenOdd from '../assets/tray/categories/evenOdd.svg';
+import fractions from '../assets/tray/categories/fractions.svg';
+import hundredTable from '../assets/tray/categories/hundredTable.svg';
+import markGame from '../assets/tray/categories/markGame.svg';
+import numberCards from '../assets/tray/categories/numberCards.svg';
+import pearls from '../assets/tray/categories/pearls.svg';
+import redBlueBars from '../assets/tray/categories/redBlueBars.svg';
+import seguinTable from '../assets/tray/categories/seguinTable.svg';
+import stripSum from '../assets/tray/categories/stripSum.svg';
 
 export class MontessoriItemsGenerator {
     public static readonly [NOT_CONSTRUCTABLE] = true;
@@ -61,70 +67,74 @@ export class MontessoriItemsGenerator {
         return [
             {
                 title: <Translate name={`Montessori / red blue bars`}>Červeno-modré tyče</Translate>,
-                icon: 'redBlueBars.svg',
+                icon: redBlueBars,
                 scale: 0.5,
                 items: toolbarRedBlueBars(),
             },
             {
                 title: <Translate name={`Montessori / pearls`}>Perlový materiál</Translate>,
-                icon: 'pearls.svg',
+                icon: pearls,
                 scale: 0.6,
                 items: toolbarPearls(),
             },
             {
                 title: <Translate name={`Montessori / arrows`}>Šipky k perlovému materiálu</Translate>,
-                icon: 'arrows.svg',
+                icon: arrows,
                 scale: 0.6,
                 items: toolbarArrows(),
             },
             {
                 title: <Translate name={`Montessori / number cards`}>Karty s čísly</Translate>,
-                icon: 'numberCards.svg',
+                icon: numberCards,
                 scale: 0.5,
                 items: toolbarNumberCards(),
             },
             {
                 title: <Translate name={`Montessori / even odd`}>Sudá a lichá</Translate>,
-                icon: 'evenOdd.svg',
+                icon: evenOdd,
                 scale: 0.9,
                 items: toolbarEvenOdd(),
             },
             {
                 title: <Translate name={`Montessori / strip sum`}>Sčítací proužková tabulka</Translate>,
-                icon: 'stripSum.svg',
+                icon: stripSum,
                 scale: 0.5,
                 items: toolbarStripSum(),
             },
             {
                 title: <Translate name={`Montessori / hundred board`}>Stovková tabulka</Translate>,
-                icon: 'hundredTable.svg',
+                icon: hundredTable,
                 scale: 0.6,
                 items: toolbarHundredBoard(),
             },
             {
                 title: <Translate name={`Montessori / seguin board`}>Seguinova tabulka</Translate>,
-                icon: 'seguinTable.svg',
+                icon: seguinTable,
                 scale: 0.5,
                 items: toolbarSeguinBoard(),
             },
             {
                 title: <Translate name={`Montessori / mark game`}>Známková hra</Translate>,
-                icon: 'markGame.svg',
+                icon: markGame,
                 scale: 0.9,
                 items: toolbarMarkGame(),
             } /*
             {
                 name: <Translate name={`Montessori / abacus`}>Počítadlo</Translate>,
-                icon: 'abacus.svg',
+                icon: abacus,
                 scale: 1,
                 items: [],
             },*/,
             {
                 title: <Translate name={`Montessori / fractions`}>Zlomky</Translate>,
-                icon: 'fractions.svg',
+                icon: fractions,
                 scale: 0.35,
                 items: toolbarFractions(),
             },
         ];
     }
 }
+
+/**
+ * TODO: Make translations inside this repository
+ */
